99a888114af2648b16141b4c8fa9d866002de6c7
Mercy <aprilkasha42@gmail.com>
2020-08-01 13:48:32 +0300

    create a def search_results function

--- galleria/views.py
+++ galleria/views.py
@@ -7,3 +7,14 @@ def gallery(request):
     print(all_pics)
     return render(request, 'gallery.html',{"all_pics":all_pics})
     
+def search_results(request):
+    if 'image' in request.GET and request.GET['image']:
+        search_input = request.GET.get('image')
+        searched_images = Image.search_by_category(search_input)
+        message = f"{search_input}"
+
+        return render(request, 'search.html', {"message":message, "images":searched_images})
+
+    else:
+        message = "Please input something in the search field"
+        return render(request, 'search.html', {'message':message})
