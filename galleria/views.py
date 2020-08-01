ffa92440ba6635481f5e1905d9bf67bea904fd72
Mercy <aprilkasha42@gmail.com>
2020-08-01 13:46:15 +0300

    add templates

--- galleria/views.py
+++ galleria/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to my photo gallery')
+def gallery(request):
+    all_pics = Image.all_pics()
+    print(all_pics)
+    return render(request, 'gallery.html',{"all_pics":all_pics})
     
